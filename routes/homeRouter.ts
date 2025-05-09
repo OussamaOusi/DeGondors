4be85d5363a0e28c201c88012d54fbea2bf9e1e7
Robin-dotcom <72873870+Robin-dotcom@users.noreply.github.com>
2025-05-09 08:24:43 +0000

    Add secureMiddleware to homepage

--- routes/homeRouter.ts
+++ routes/homeRouter.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from 'express';
+import { secureMiddleware } from '../secureMiddleware';
 
 const router = Router();
 
-router.get('/home', (_req: Request, res: Response) => {
-  res.render('home');
+router.get('/home', secureMiddleware, async (_req: Request, res: Response) => {
+  res.render('home', {user: _req.session.user});
 });
 
 export default router;
